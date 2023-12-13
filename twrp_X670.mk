a99015c0d4633758a2ceb7f2f194c6de32584d34
Ardiandi Deya Shidiq <116504807+ardiandideyashidiq@users.noreply.github.com>
2023-12-13 23:48:34 +0700

    Update twrp_X670.mk

--- twrp_X670.mk
+++ twrp_X670.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from Infinix-X670 device
-$(call inherit-product, device/infinix/Infinix-X670/device.mk)
+# Inherit from X670 device
+$(call inherit-product, device/infinix/X670/device.mk)
 
 PRODUCT_DEVICE := X670
 PRODUCT_NAME := twrp_X670
