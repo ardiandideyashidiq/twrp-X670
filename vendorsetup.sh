69ee7cfe3b91d21042d275713c77f77b4473ed0e
Ardiandi Deya Shidiq <116504807+ardiandideyashidiq@users.noreply.github.com>
2023-12-13 23:47:19 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X670-user
-add_lunch_combo omni_Infinix-X670-userdebug
-add_lunch_combo omni_Infinix-X670-eng
+add_lunch_combo twrp_X670-user
+add_lunch_combo twrp_X670-userdebug
+add_lunch_combo twrp_X670-eng
